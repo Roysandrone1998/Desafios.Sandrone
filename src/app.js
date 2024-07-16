354243bfd3247393057f9e596b39dac49b9a2318
Roysandrone1998 <raysandrone@gmail.com>
2024-07-15 22:22:39 -0300

    arreglando base de datos2

--- src/app.js
+++ src/app.js
@@ -6,39 +6,35 @@ const passport = require("passport");
 const initializePassport = require("./config/passport.config.js");
 const cors = require("cors");
 const path = require('path');
-const PUERTO = process.env.PUERTO || 8080;
+const PUERTO = 8080;
 require("./database.js");
-
+require('dotenv').config();
 
 const productsRouter = require("./routes/products.router.js");
 const cartsRouter = require("./routes/carts.router.js");
 const viewsRouter = require("./routes/views.router.js");
 const userRouter = require("./routes/user.router.js");
 
-//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-//Passport 
 app.use(passport.initialize());
 initializePassport();
 app.use(cookieParser());
 
-//AuthMiddleware
+
 const authMiddleware = require("./middleware/authmiddleware.js");
 app.use(authMiddleware);
 
 
-//Handlebars
+
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 app.set("views", "./src/views");
 
-
-//Rutas: 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/api/users", userRouter);
@@ -48,6 +44,24 @@ const httpServer = app.listen(PUERTO, () => {
     console.log(`Servidor escuchando en el puerto ${PUERTO}`);
 });
 
-///Websockets: 
 const SocketManager = require("./sockets/socketmanager.js");
 new SocketManager(httpServer);
+
+
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUiExpress = require('swagger-ui-express');
+
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.1",
+        info: {
+            title: "Documentacion de la App Tienda TechZone",
+            description: "E-commerce"
+        }
+    },
+    apis: ["./src/docs/**/*.yaml"]
+    
+}
+
+const specs = swaggerJSDoc(swaggerOptions);
+app.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
--- src/database.js
+++ src/database.js
@@ -1,23 +1,6 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
-const configObject = require("./config/config.js");
-const {mongo_url} = configObject;
 
-class BaseDatos {
-    static #instancia; 
-    constructor(){
-        mongoose.connect("mongodb+srv://roysandrone:Coder1@cluster0.ybo821i.mongodb.net/ecomerce?retryWrites=true&w=majority&appName=Cluster0s");
-    }
-
-    static getInstancia() {
-        if(this.#instancia) {
-            console.log("Conexion previa");
-            return this.#instancia;
-        }
-
-        this.#instancia = new BaseDatos();
-        console.log("Conexión exitosa!!");
-        return this.#instancia;
-    }
-}
-
-module.exports = BaseDatos.getInstancia();
+mongoose.connect(process.env.MONGO_URL)
+    .then( () => console.log("Conexion exitosa a la BD"))
+    .catch( (error) => console.log(error))
